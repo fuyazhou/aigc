0210bd68cdbb4dc99658fd381dc6800206322a88
yazhou <fuyazhou80@hotmail.com>
2023-06-10 13:37:09 +0800

    change log

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ chat = ChatOpenAI(
 # chat = chat
 # embeddings = embeddings
 
+def append_to_file(file_path, new_content):
+    with open(file_path, 'a', encoding="utf_8") as f:
+        f.write(new_content)
+
 
 def generate_data(query,
                   data_path: str = database_path,
@@ -82,7 +86,9 @@ def generate_article(query,
     logger.info("Generating article for query: {}".format(query))
 
     try:
+        print("\n\n==================================\n\n")
         print("Generating article for query: {}".format(query))
+        print("\n\n==================================\n\n")
 
         # Perform similarity search.
         logger.info("Performing similarity search")
@@ -109,19 +115,29 @@ def generate_article(query,
         logger.info("Generating chat messages")
         article = chat_with_model(generate_prompt, summary)
         print("\n\n==================================\n\n")
-        print("first Generated article for query: {}".format(article))
-
-        logger.info("Generating chat messages again")
-        article = chat_with_model(generate_prompt_2, article)
+        print("first Generated article for query:\n\n {}".format(article))
         print("\n\n==================================\n\n")
-        print("second  Generated article for query: {}".format(article))
+
+        logger.info("****Generating chat messages again***")
+        article = chat_with_model(generate_prompt_2, article)
+
+        logger.info("****translate  article to Chinese* **")
+        cn_article = chat_with_model("You are a helpful assistant that translates English to Chinese.", article)
+
+        print("\n\n==================================\n\n")
+        print("second  Generated article for query:\n\n {}".format(article))
+        print("\n\n==================================\n\n")
+        print("\n\n==================================\n\n")
+        print("translate second  Generated article into chinese:\n {}".format(cn_article))
+        print("\n\n==================================\n\n")
 
         logger.info("Article fetched successfully")
         # Log the end of the function.
         logger.info("Generated article for query: {}".format(article))
+        logger.info("translate second  Generated article into chinese:\n {}".format(cn_article))
 
         # 保存结果到数据库
-        article_dict = {"query": [query], "summary": [summary], "article": [article]}
+        article_dict = {"query": [query], "summary": [summary], "article": [article + "\n\n\n\n" + cn_article]}
         df = pd.DataFrame.from_dict(article_dict)
 
         if os.path.isfile(article_path):
@@ -131,7 +147,13 @@ def generate_article(query,
         logger.info("Saving df to article path")
         df.to_csv(article_path, index=False, encoding="utf_8")
 
-        return article
+        logger.info("save article  to  data/article.txt")
+        append_to_file(article_txt_path, "****************\n")
+        append_to_file(article_txt_path, query + ":\n\n")
+        append_to_file(article_txt_path, article + "\n\n")
+        append_to_file(article_txt_path, cn_article + "\n\n")
+
+        return article + "\n\n\n\n" + cn_article
     except Exception as e:
         logger.warning("Generating article something wrong")
         logger.warning(f"An error occurred: {str(e)}")
@@ -163,22 +185,25 @@ def main(generate_data_query: str = "None", generate_article_query: str = "None"
         print("\n==================================\n")
         print(f"start generate data query is {generate_data_query}")
         data = generate_data(generate_data_query)
-        print(f"generate data done, data =  {data}")
+        # data = "\n".join(data)
+        # print(f"generate data done, data =  {data}")
     if generate_article_query != "None":
-        print("\n==================================\n")
-        print(f"start article data query is {generate_article_query}")
+        # print("\n==================================\n")
+        # print(f"start article data query is {generate_article_query}")
         data = generate_article(generate_article_query)
-        print("\n\n==================================\n\n")
-        print(f"generate article done, data =\n\n  {data}")
-        print("\n\n")
+        # print("\n\n==================================\n\n")
+        # print(f"generate article done, data =\n\n  {data}")
+        # print("\n\n")
 
     if generate_data_query == "None" and generate_article_query == "None":
         print("\n==================================\n")
         print("please input generate_data_query or generate_article_query")
 
 
-# main.py --generate_data_query 知乎的商業模式 --generate_article_query 知乎的商業模式
+# main.py --generate_data_query Apple company business model --generate_article_query Apple company business model
 if __name__ == '__main__':
     fire.Fire(main)
-    # generate_data('知乎的商業模式')
-    # article = generate_article('知乎的商業模式')
+    # generate_data('Apple company business model')
+    # article = generate_article('Apple company business model')
+
+# python main.py --generate_data_query Apple company business model
