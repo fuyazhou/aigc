a6fc069b55341bfb5d6961d60221e0ebb837289e
Yazhou Fu <fuyazhou80@hotmail.com>
2023-06-26 11:46:20 +0800

    stable pretrain

---
+++ predict.py
@@ -0,0 +1,27 @@
+
+
+# if Checkpoints only save the unet
+import torch
+from diffusers import StableDiffusionPipeline, UNet2DConditionModel
+
+model_path = "/content/diffusers/examples/text_to_image/sd-pokemon-model/checkpoint-15"
+
+unet = UNet2DConditionModel.from_pretrained(model_path + "/unet")
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", unet=unet)
+
+pipe.to("cuda")
+image = pipe(prompt="").images[0]
+image.save("image.png")
+
+
+
+# if last Checkpoints  saved
+from diffusers import StableDiffusionPipeline
+
+model_path = "path_to_saved_model"
+pipe = StableDiffusionPipeline.from_pretrained(model_path)
+pipe.to("cuda")
+
+image = pipe(prompt="").images[0]
+image.save("image.png")
+
