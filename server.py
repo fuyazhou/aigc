c28435ab8e44033d9a2849bc8a98f9ed41bc068f
Yazhou Fu <fuyazhou80@hotmail.com>
2023-07-04 14:59:04 +0800

    change server

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+
+import main_webpilot
 from main import *
 
 app = Flask(__name__)
@@ -11,6 +13,17 @@ def generate_article():
     if generate_data_query:
         summary = generate_data(generate_data_query)
         article = generate_article(summary)
+        return {'article': article}
+
+    return {'error': 'Please provide a generate_data_query parameter.'}
+
+
+@app.route('/generate_webpilot', methods=['POST'])
+def generate_webpilot():
+    generate_data_query = request.json.get('generate_data_query')
+
+    if generate_data_query:
+        article = main_webpilot.main(generate_data_query)
         return {'article': article}
 
     return {'error': 'Please provide a generate_data_query parameter.'}
